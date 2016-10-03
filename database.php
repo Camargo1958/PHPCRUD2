f82e5b5862098030c014541a50bbca208d0a5076
Neo <Neo@W-Box1>
2016-10-03 14:19:50 -0300

    Commit de sincronização de fontes

---
+++ database.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of database
+ *
+ * @author Neo
+ */
+class database {
+private static $dbName = 'xipbu019_customers';
+    private static $dbHost = 'br702.hostgator.com.br';
+    private static $dbUsername = 'xipbu019_admin';
+    private static $dbUserPassword = '@12345$';
+     
+    private static $cont  = null;
+     
+    public function __construct() {
+        die('Init function is not allowed');
+    }
+     
+    public static function connect()
+    {
+       // One connection through whole application
+       if ( null == self::$cont )
+       {     
+        try
+        {
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
+        }
+        catch(PDOException $e)
+        {
+          die($e->getMessage()); 
+        }
+       }
+       return self::$cont;
+    }
+     
+    public static function disconnect()
+    {
+        self::$cont = null;
+    }
+
+}
